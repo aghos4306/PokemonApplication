a2db8fcc839670f84d8f7531f133c45010e57f38
Aghogho <aghogho.akpedeye@gmail.com>
2022-07-03 20:38:37 +0100

    refactor pokemonDetailScreenRoute to accommodate passing arguments to route

--- app/src/main/java/com/aghogho/pokemonapp/navigation/PokemonNavigation.kt
+++ app/src/main/java/com/aghogho/pokemonapp/navigation/PokemonNavigation.kt
@@ -1,8 +1,10 @@
 package com.aghogho.pokemonapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
 import com.aghogho.pokemonapp.screens.PokemonDetailScreen
 import com.aghogho.pokemonapp.screens.PokemonListScreen
@@ -23,8 +25,14 @@ fun PokemonNavigation() {
             PokemonListScreen(navController = navController)
         }
 
-        composable(PokemonScreens.PokemonDetailScreen.name) {
-            PokemonDetailScreen(navController = navController)
+        //set route to navigate to specific pokemon
+        val pokemonName = PokemonScreens.PokemonDetailScreen.name
+        composable("$pokemonName/{pokemonId}", arguments = listOf(navArgument("pokemonId") {
+            type = NavType.StringType
+        })) { backStackEntry ->
+            backStackEntry.arguments?.getString("pokemonId").let {
+                PokemonDetailScreen(navController = navController, pokemonId = it.toString())
+            }
         }
     }
 }
--- app/src/main/java/com/aghogho/pokemonapp/screens/PokemonDetailScreen.kt
+++ app/src/main/java/com/aghogho/pokemonapp/screens/PokemonDetailScreen.kt
@@ -3,6 +3,7 @@ package com.aghogho.pokemonapp.screens
 import androidx.navigation.NavController
 
 class PokemonDetailScreen(
-    navController: NavController
+    navController: NavController,
+    pokemonId: String
 ) {
 }
