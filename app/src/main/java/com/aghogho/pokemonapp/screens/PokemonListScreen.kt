4ef76fab391caddb089bdf35a43fef7b83f07113
Aghogho <aghogho.akpedeye@gmail.com>
2022-07-05 00:08:07 +0100

    created search bar and common pokemon in pokemonListScreen

--- app/src/main/java/com/aghogho/pokemonapp/screens/PokemonListScreen.kt
+++ app/src/main/java/com/aghogho/pokemonapp/screens/PokemonListScreen.kt
@@ -1,14 +1,89 @@
 package com.aghogho.pokemonapp.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.aghogho.pokemonapp.R
 
 @Composable
 fun PokemonListScreen(
     navController: NavController
 ) {
     
-    Text(text = "Pokemon List Screen")
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(18.dp))
+            Image(painter = painterResource(id = R.drawable.ic_common_pokemon),
+                contentDescription = "Pokemon",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(CenterHorizontally)
+            )
+            PokemonSearchBar()
+        }
+    }
     
+}
+
+@Composable
+fun PokemonSearchBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var searchText by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = searchText,
+            onValueChange = {
+                searchText = it
+                onSearch(it)
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(4.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    //isHintDisplayed = it != FocusState.Active
+                }
+        )
+        if (isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
 }
